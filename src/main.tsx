4a3c97d82b73aa41b09e6e657ebe7060dc66372a
islom-sattorov <slmsttrv1@gmail.com>
2024-06-06 10:06:33 +0500

    set up github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import { ColorModeScript } from "@chakra-ui/react";
-import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Providers } from "./providers/index.tsx";
 import theme from "./utils/theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Providers>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    </Providers>
-  </StrictMode>
+  <Providers>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+  </Providers>
 );
